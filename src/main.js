67f2939e2db1b46452b9978307d9da90fb2114d1
juan2ramos <juan2ramos@gmail.com>
2018-02-24 18:32:19 -0500

    full whitout vuex

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from '@/App.vue'
 
+
 import routes from '@/routes'
+import store from '@/store'
 import EventBus from '@/plugins/event-bus'
 import msToMm from '@/filters/ms-to-mm'
 import blur from '@/directives/blur'
@@ -17,5 +19,6 @@ const router = new VueRouter({ routes, mode: 'history' });
 new Vue({
   el: '#app',
   render: h => h(App),
-  router
+  router,
+  store
 });
